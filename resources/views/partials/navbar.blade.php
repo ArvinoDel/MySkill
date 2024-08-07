cc162e49c8743cda3628402718ae052f30fe9c03
kulkad <rdarmawan281@gmail.com>
2024-08-07 14:22:52 +0700

    fix

--- resources/views/partials/navbar.blade.php
+++ resources/views/partials/navbar.blade.php
@@ -36,64 +36,25 @@
               <a href="/team" class="rounded-md {{ request()->is('team') ? 'underline underline-black underline-offset text-black' : 'text-white' }} px-3 py-2 text-lg">Team</a>
               <a href="/projects" class="rounded-md {{ request()->is('projects') ? 'underline underline-black underline-offset text-black' : 'text-white' }} px-3 py-2 text-lg">Projects</a>
               <a href="/calendar" class="rounded-md {{ request()->is('calendar') ? 'underline underline-black underline-offset text-black' : 'text-white' }} px-3 py-2 text-lg">Calendar</a>
-        <div class="relative flex h-16 items-center justify-between">
-            <div class="absolute inset-y-0 left-0 flex items-center sm:hidden">
-                <!-- Mobile menu button -->
-                <button id="mobile-menu-button" type="button"
-                    class="relative inline-flex items-center justify-center rounded-md p-2 text-gray-400 hover:bg-gray-700 hover:text-white focus:outline-none focus:ring-2 focus:ring-inset focus:ring-white"
-                    aria-controls="mobile-menu" aria-expanded="false">
-                    <span class="sr-only">Open main menu</span>
-                    <!-- Icon when menu is closed -->
-                    <svg class="block h-6 w-6" fill="none" viewBox="0 0 24 24" stroke-width="1.5"
-                        stroke="currentColor" aria-hidden="true">
-                        <path stroke-linecap="round" stroke-linejoin="round"
-                            d="M3.75 6.75h16.5M3.75 12h16.5m-16.5 5.25h16.5" />
-                    </svg>
-                    <!-- Icon when menu is open -->
-                    <svg class="hidden h-6 w-6" fill="none" viewBox="0 0 24 24" stroke-width="1.5"
-                        stroke="currentColor" aria-hidden="true">
-                        <path stroke-linecap="round" stroke-linejoin="round" d="M6 18L18 6M6 6l12 12" />
-                    </svg>
-                </button>
             </div>
-
-            <div class="flex flex-1 items-center justify-between sm:items-stretch sm:justify-start">
-                <div class="flex flex-shrink-0 items-center">
-                    <p class="text-lg sm:text-xl font-semibold text-white">GMTrainer</p>
-                </div>
-                <div class="hidden sm:ml-9 sm:block">
-                    <div class="flex space-x-4">
-                        <a href="/"
-                            class="rounded-md {{ request()->is('/') ? 'underline underline-black underline-offset-4 text-black' : 'text-white' }} px-3 py-2 text-lg"
-                            aria-current="page">Dashboard</a>
-                        <a href="/team"
-                            class="rounded-md {{ request()->is('team') ? 'underline underline-black underline-offset-4 text-black' : 'text-white' }} px-3 py-2 text-lg">Team</a>
-                        <a href="/projects"
-                            class="rounded-md {{ request()->is('projects') ? 'underline underline-black underline-offset-4 text-black' : 'text-white' }} px-3 py-2 text-lg">Projects</a>
-                        <a href="/calendar"
-                            class="rounded-md {{ request()->is('calendar') ? 'underline underline-black underline-offset-4 text-black' : 'text-white' }} px-3 py-2 text-lg">Calendar</a>
-                    </div>
-                </div>
-            </div>
-            <div class="absolute inset-y-0 right-0 flex items-center pr-2 sm:static sm:inset-auto sm:ml-6 sm:pr-0">
+          </div>
+        </div>
+        <div class="absolute inset-y-0 right-0 flex items-center pr-2 sm:static sm:inset-auto sm:ml-6 sm:pr-0">
                 <button class="border border-rose-600 text-rose-600 me-4 px-7 py-2 rounded-md text-md">Login</button>
                 <button class="bg-rose-600 text-white px-7 py-2 rounded-md text-md">Register</button>
-            </div>
+          </div>
         </div>
+      </div>
     </div>
 
-    <!-- Mobile menu, show/hide based on menu state -->
-    <div id="mobile-menu" class="sm:hidden hidden">
-        <div class="space-y-1 px-2 pb-3 pt-2">
-            <a href="/"
-                class="block rounded-md {{ request()->is('/') ? 'bg-gray-900 text-white' : 'text-gray-300 hover:bg-gray-700 hover:text-white' }} px-3 py-2 text-base font-medium"
-                aria-current="page">Dashboard</a>
-            <a href="/team"
-                class="block rounded-md {{ request()->is('team') ? 'bg-gray-900 text-white' : 'text-gray-300 hover:bg-gray-700 hover:text-white' }} px-3 py-2 text-base font-medium">Team</a>
-            <a href="/projects"
-                class="block rounded-md {{ request()->is('projects') ? 'bg-gray-900 text-white' : 'text-gray-300 hover:bg-gray-700 hover:text-white' }} px-3 py-2 text-base font-medium">Projects</a>
-            <a href="/calendar"
-                class="block rounded-md {{ request()->is('calendar') ? 'bg-gray-900 text-white' : 'text-gray-300 hover:bg-gray-700 hover:text-white' }} px-3 py-2 text-base font-medium">Calendar</a>
-        </div>
+    <!-- Mobile menu, show/hide based on menu state. -->
+    <div class="sm:hidden" id="mobile-menu">
+      <div class="space-y-1 px-2 pb-3 pt-2">
+        <!-- Current: "bg-gray-900 text-white", Default: "text-gray-300 hover:bg-gray-700 hover:text-white" -->
+        <a href="#" class="block rounded-md bg-gray-900 px-3 py-2 text-base font-medium text-white" aria-current="page">Dashboard</a>
+        <a href="#" class="block rounded-md px-3 py-2 text-base font-medium text-gray-300 hover:bg-gray-700 hover:text-white">Team</a>
+        <a href="#" class="block rounded-md px-3 py-2 text-base font-medium text-gray-300 hover:bg-gray-700 hover:text-white">Projects</a>
+        <a href="#" class="block rounded-md px-3 py-2 text-base font-medium text-gray-300 hover:bg-gray-700 hover:text-white">Calendar</a>
+      </div>
     </div>
-</nav>
+  </nav>
