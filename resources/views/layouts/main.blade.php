06af5ccceee058a7abfa3ba170c4f754d4c1f12e
Andika <andikasupriyadi27@gmail.com>
2024-08-07 12:58:10 +0700

    "Added inline style to set body background color and included navbar partial in main layout"

--- resources/views/layouts/main.blade.php
+++ resources/views/layouts/main.blade.php
@@ -7,10 +7,16 @@
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <title>Document</title>
     @vite('resources/css/app.css')
+    <style>
+        body {
+            background-color: #e09129 !important;
+        }
+    </style>
 
 </head>
 
 <body>
+    @include('partials.navbar')
     <div class="container" id="container">
         @yield('container')
     </div>
