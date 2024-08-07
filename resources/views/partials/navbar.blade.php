8afca24d1999c10338833fe24cb730c68574148d
kulkad <rdarmawan281@gmail.com>
2024-08-07 14:07:29 +0700

    update

--- resources/views/partials/navbar.blade.php
+++ resources/views/partials/navbar.blade.php
@@ -24,9 +24,9 @@
             </svg>
           </button>
         </div>
+
         <div class="flex flex-1 items-center justify-center sm:items-stretch sm:justify-start">
           <div class="flex flex-shrink-0 items-center">
-
             <p class="text-xl font-semibold">GMTrainer</p>
           </div>
           <div class="hidden sm:ml-6 sm:block">
