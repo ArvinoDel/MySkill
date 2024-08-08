4dd46de4cde7e08f01d3bb33924d23ee3b6fdf72
Andika <andikasupriyadi27@gmail.com>
2024-08-08 13:40:01 +0700

    update

--- resources/views/layouts/main.blade.php
+++ resources/views/layouts/main.blade.php
@@ -2,27 +2,23 @@
 <html lang="en">
 
 <head>
-  <meta charset="UTF-8">
-  <meta name="viewport" content="width=device-width, initial-scale=1.0">
-  <meta http-equiv="X-UA-Compatible" content="ie=edge">
-  <title>MySkill</title>
-  @vite('resources/css/app.css')
-  @vite('resources/js/navbar.js')
-  {{-- <style>
-        body {
-            background-color: #e09129 !important;
-        }
-    </style> --}}
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>MySkill</title>
+    @vite('resources/css/app.css')
+    @vite('resources/js/navbar.js')
+    <link href='https://unpkg.com/boxicons@2.1.4/css/boxicons.min.css' rel='stylesheet'>
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.4/css/all.min.css">
 </head>
 
 <body>
-  @include('partials.navbar')
-  <div class="container" id="container">
-    @yield('container')
-  </div>
+    @include('partials.navbar')
+    <div class="container" id="container">
+        @yield('container')
+    </div>
 
-  @include('partials.footer')
+    @include('partials.footer')
 
 </body>
 
