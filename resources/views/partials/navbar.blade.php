2658d79ee1e4aa27e97a9adfcdf863b2f494e843
Andika <andikasupriyadi27@gmail.com>
2024-08-08 08:34:12 +0700

    "Updated package-lock.json and package.json to use tailwindcss 3.4.8, refactored navbar and layout in resources/views, and modified CSS in resources/css/app.css"

--- resources/views/partials/navbar.blade.php
+++ resources/views/partials/navbar.blade.php
@@ -1,61 +1,113 @@
-<nav class="bg-orange-400">
-    <div class="mx-auto max-w-7xl px-2 sm:px-6 lg:px-8">
-      <div class="relative flex h-16 items-center justify-between">
-        <div class="absolute inset-y-0 left-0 flex items-center sm:hidden">
-          <!-- Mobile menu button-->
-          <button type="button" class="relative inline-flex items-center justify-center rounded-md p-2 text-gray-400  hover:text-white focus:outline-none focus:ring-2 focus:ring-inset focus:ring-white" aria-controls="mobile-menu" aria-expanded="false">
-            <span class="absolute -inset-0.5"></span>
-            <span class="sr-only">Open main menu</span>
-            <!--
-              Icon when menu is closed.
-
-                Menu open: "hidden", Menu closed: "block"
-              -->
-              <svg class="block h-6 w-6" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" aria-hidden="true">
-                <path stroke-linecap="round" stroke-linejoin="round" d="M3.75 6.75h16.5M3.75 12h16.5m-16.5 5.25h16.5" />
-              </svg>
-              <!--
-                Icon when menu is open.
-
-                Menu open: "block", Menu closed: "hidden"
-              -->
-              <svg class="hidden h-6 w-6" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" aria-hidden="true">
-                <path stroke-linecap="round" stroke-linejoin="round" d="M6 18L18 6M6 6l12 12" />
-              </svg>
+<nav class="relative px-8 py-4 flex justify-between items-center bg-white">
+    <a class="text-3xl font-bold leading-none" href="#">
+        <svg xmlns="http://www.w3.org/2000/svg" class="h-8 w-8 hover:text-green-600" viewBox="0 0 20 20"
+            fill="currentColor">
+            <path fill-rule="evenodd"
+                d="M11.3 1.046A1 1 0 0112 2v5h4a1 1 0 01.82 1.573l-7 10A1 1 0 018 18v-5H4a1 1 0 01-.82-1.573l7-10a1 1 0 011.12-.38z"
+                clip-rule="evenodd" />
+        </svg>
+    </a>
+    <div class="lg:hidden">
+        <button class="navbar-burger flex items-center p-3 focus:outline-none hover:text-green-500 focus:text-gray-50">
+            <svg xmlns="http://www.w3.org/2000/svg" class="h-5 w-5 fill-current" viewBox="0 0 20 20"
+                fill="currentColor">
+                <path fill-rule="evenodd"
+                    d="M3 5a1 1 0 011-1h12a1 1 0 110 2H4a1 1 0 01-1-1zM3 10a1 1 0 011-1h12a1 1 0 110 2H4a1 1 0 01-1-1zM9 15a1 1 0 011-1h6a1 1 0 110 2h-6a1 1 0 01-1-1z"
+                    clip-rule="evenodd" />
+            </svg>
+        </button>
+    </div>
+    <ul
+        class="hidden absolute top-1/2 left-1/2 transform -translate-y-1/2 -translate-x-1/2 lg:mx-auto lg:flex lg:items-center lg:w-auto lg:space-x-8">
+        <li>
+            <a class="text-sm text-gray-700 font-medium hover:text-green-500 items-center hover:border border-green-600 px-2 py-2 rounded hover:shadow"
+                href="#">
+                News
+            </a>
+        </li>
+        <li>
+            <a class="text-sm text-gray-700 font-medium hover:text-green-500 items-center hover:border border-green-600 px-2 py-2 rounded hover:shadow"
+                href="#">
+                About Us
+            </a>
+        </li>
+        <li>
+            <a class="text-sm text-gray-700 font-medium hover:text-green-500 items-center hover:border border-green-600 px-2 py-2 rounded hover:shadow"
+                href="#">
+                Lectures
+            </a>
+        </li>
+        <li>
+            <a class="text-sm text-gray-700 font-medium hover:text-green-500 items-center hover:border border-green-600 px-2 py-2 rounded hover:shadow"
+                href="#">
+                Contacts
+            </a>
+        </li>
+    </ul>
+    <button
+        class="hidden lg:inline-block py-2 px-8 bg-white hover:bg-green-600 text-sm text-center text-gray-800 hover:text-white font-bold border-2 border-gray-800 hover:border-green-600 hover:shadow-md
+    transform active:scale-75 transition-transform focus:border-4 focus:border-green-300 duration-700 focus:outline-none">Log
+        In</button>
+</nav>
+<div class="navbar-menu relative z-50 hidden">
+    <div class="navbar-backdrop fixed inset-0 bg-gray-800 opacity-25"></div>
+    <nav class="fixed top-0 left-0 bottom-0 flex flex-col w-5/6 max-w-sm py-6 px-6 bg-white border-r overflow-y-auto">
+        <div class="flex items-center mb-8">
+            <a class="mr-auto text-3xl font-bold leading-none hover:text-green-600" href="#">
+                <svg xmlns="http://www.w3.org/2000/svg" class="h-8 w-8" viewBox="0 0 20 20" fill="currentColor">
+                    <path fill-rule="evenodd"
+                        d="M11.3 1.046A1 1 0 0112 2v5h4a1 1 0 01.82 1.573l-7 10A1 1 0 018 18v-5H4a1 1 0 01-.82-1.573l7-10a1 1 0 011.12-.38z"
+                        clip-rule="evenodd" />
+                </svg>
+            </a>
+            <button class="navbar-close focus:outline-none">
+                <svg class="h-6 w-6 text-gray-400 cursor-pointer hover:text-green-500 "
+                    xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke="currentColor">
+                    <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M6 18L18 6M6 6l12 12">
+                    </path>
+                </svg>
             </button>
-          </div>
-
-        <div class="grid grid-flow-col items-center justify-center sm:items-stretch">
-          <div class="flex flex-shrink-0 items-center sm:justify-center">
-            <p class="text-xl font-semibold ps-12">GMTrainer</p>
-          </div>
-          <div class="hidden sm:ml-6 sm:block">
-            <div class="flex space-x-4 ps-56">
-              <!-- Current: "bg-gray-900 text-white", Default: "text-gray-300 hover:bg-gray-700 hover:text-white" -->
-              <a href="/" class="rounded-md {{ request()->is('/') ? 'underline underline-black underline-offset text-black' : 'text-white' }} px-3 py-2 text-lg" aria-current="page">Dashboard</a>
-              <a href="/team" class="rounded-md {{ request()->is('team') ? 'underline underline-black underline-offset text-black' : 'text-white' }} px-3 py-2 text-lg">Team</a>
-              <a href="/projects" class="rounded-md {{ request()->is('projects') ? 'underline underline-black underline-offset text-black' : 'text-white' }} px-3 py-2 text-lg">Projects</a>
-              <a href="/calendar" class="rounded-md {{ request()->is('calendar') ? 'underline underline-black underline-offset text-black' : 'text-white' }} px-3 py-2 text-lg">Calendar</a>
+        </div>
+        <div>
+            <ul>
+                <li class="mb-1 inline-flex group hover:bg-green-50 rounded w-full">
+                    <div
+                        class="flex w-1 group-hover:bg-green-500 scale-y-0 group-hover:scale-100 transition-transform origin-top rounded-full duration-400 ease-in">
+                    </div>
+                    <a class="flex p-4 text-sm font-semibold text-gray-500 group-hover:text-green-600"
+                        href="#">News</a>
+                </li>
+                <li class="mb-1 inline-flex group hover:bg-green-50 rounded w-full">
+                    <div
+                        class="flex w-1 group-hover:bg-green-500 scale-y-0 group-hover:scale-100 transition-transform origin-top rounded-full duration-400 ease-in">
+                    </div>
+                    <a class="block p-4 text-sm font-semibold text-gray-500 group-hover:text-green-600"
+                        href="#">About Us</a>
+                </li>
+                <li class="mb-1 inline-flex group hover:bg-green-50 rounded w-full">
+                    <div
+                        class="flex w-1 group-hover:bg-green-500 scale-y-0 group-hover:scale-100 transition-transform origin-top rounded-full duration-400 ease-in">
+                    </div>
+                    <a class="block p-4 text-sm font-semibold text-gray-500 group-hover:text-green-600"
+                        href="#">Lectures</a>
+                </li>
+                <li class="mb-1 inline-flex group hover:bg-green-50 rounded w-full">
+                    <div
+                        class="flex w-1 group-hover:bg-green-500 scale-y-0 group-hover:scale-100 transition-transform origin-top rounded-full duration-400 ease-in">
+                    </div>
+                    <a class="block p-4 text-sm font-semibold text-gray-500 group-hover:text-green-600"
+                        href="#">Contacts</a>
+                </li>
+            </ul>
+        </div>
+        <div class="mt-auto">
+            <div class="pt-6">
+                <button
+                    class="w-full py-3 px-4 bg-white hover:bg-green-600 text-sm text-center text-gray-800 hover:text-white font-bold border-2 border-gray-800 hover:border-green-600 hover:shadow-md
+          transform active:scale-75 transition-transform focus:border-4 focus:border-green-300 duration-700 focus:outline-none">Log
+                    In</button>
             </div>
-          </div>
-        </div>
-        <div class="absolute inset-y-0 right-0 flex items-center pr-2 sm:static sm:inset-auto sm:ml-6 sm:pr-0">
-                <button class="border border-rose-600 text-rose-600 me-4 px-4 py-2 rounded-md text-md lg:px-7 ">Login</button>
-                <button class="bg-rose-600 text-white px-4 py-2 rounded-md text-md lg:px-7">Register</button>
-          </div>
-        </div>
-      </div>
-    </div>
 
-    <!-- Mobile menu, show/hide based on menu state. -->
-    <div class="fixed inset-0 z-50 bg-opacity-50 hidden" id="mobile-menu">
-        <div class="flex justify-center items-center h-full">
-            <div class="space-y-1 px-2 pb-3 pt-2 bg-orange-400 w-full text-center">
-                <a href="/" class="block rounded-md {{ request()->is('/') ? 'underline underline-black underline-offset text-black' : 'text-white' }} px-3 py-2 text-base font-medium" aria-current="page">Dashboard</a>
-                <a href="/team" class="block rounded-md {{ request()->is('team') ? 'underline underline-black underline-offset text-black' : 'text-white' }} px-3 py-2 text-base font-medium">Team</a>
-                <a href="/projects" class="block rounded-md {{ request()->is('projects') ? 'underline underline-black underline-offset text-black' : 'text-white' }} px-3 py-2 text-base font-medium">Projects</a>
-                <a href="/calendar" class="block rounded-md {{ request()->is('calendar') ? 'underline underline-black underline-offset text-black' : 'text-white' }} px-3 py-2 text-base font-medium">Calendar</a>
-            </div>
         </div>
-    </div>
-  </nav>
+    </nav>
+</div>
