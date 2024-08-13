1dc8c665e12ee7c3ae1287f121af78e3cb44a507
ryunex15 <badarudin1511@gmail.com>
2024-08-13 11:43:37 +0700

    experience page

--- resources/views/layouts/main.blade.php
+++ resources/views/layouts/main.blade.php
@@ -13,6 +13,7 @@
     <link href="https://cdn.jsdelivr.net/npm/tailwindcss@2.2.19/dist/tailwind.min.css" rel="stylesheet">
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.4.0/css/all.min.css">
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.4/css/all.min.css">
+    @vite('resources/js/buttons.js')
 </head>
 
 <body>
