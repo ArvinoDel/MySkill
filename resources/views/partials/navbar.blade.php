614c53768cb4e3a8d35e4831dd82800f193d363a
ryunex15 <badarudin1511@gmail.com>
2024-08-29 09:59:52 +0700

    nav

--- resources/views/partials/navbar.blade.php
+++ resources/views/partials/navbar.blade.php
@@ -15,51 +15,31 @@
         class="hidden absolute top-1/2 left-1/2 transform -translate-y-1/2 -translate-x-1/2 lg:mx-auto lg:flex lg:items-center lg:w-auto lg:space-x-5">
         <li>
             <a href="/home"
-<<<<<<< HEAD
-                class="text-nowrap {{ request()->is('home') ? 'text-teal-400 before:scale-x-100' : 'text-black before:scale-x-0' }} text-xs lg:text-sm font-medium hover:text-teal-400 relative px-2 py-2 rounded transition-all duration-300 before:absolute before:bottom-0 before:left-0 before:w-full before:h-0.5 before:bg-teal-400 before:scale-x-0 hover:before:scale-x-100 focus:outline-none focus:ring-0 active:text-teal-400 active:before:bg-teal-400">
-=======
                 class="text-nowrap {{ request()->is('home') ? 'text-teal-400 before:scale-x-100' : 'text-black before:scale-x-0' }} text-sm lg:text-md font-medium hover:text-teal-400 relative px-2 py-2 rounded transition-all duration-300 before:absolute before:bottom-0 before:left-0 before:w-full before:h-0.5 before:bg-teal-400 before:scale-x-0 hover:before:scale-x-100 focus:outline-none focus:ring-0 active:text-teal-400 active:before:bg-teal-400">
->>>>>>> 06d9fdd6dacfe9d4e4e10cfe34e6dae078e0db19
                 Home
             </a>
         </li>
         <li>
             <a href="/e-learning"
-<<<<<<< HEAD
-                class="text-nowrap {{ request()->is('e-learning') ? 'text-teal-400 before:scale-x-100' : 'text-black before:scale-x-0' }} text-xs lg:text-sm font-medium hover:text-teal-400 relative px-2 py-2 rounded transition-all duration-300 before:absolute before:bottom-0 before:left-0 before:w-full before:h-0.5 before:bg-teal-400 before:scale-x-0 hover:before:scale-x-100 focus:outline-none focus:ring-0 active:text-teal-400 active:before:bg-teal-400">
-=======
                 class="text-nowrap {{ request()->is('e-learning') ? 'text-teal-400 before:scale-x-100' : 'text-black before:scale-x-0' }}  text-xs lg:text-md font-medium hover:text-teal-400 relative px-2 py-2 rounded transition-all duration-300 before:absolute before:bottom-0 before:left-0 before:w-full before:h-0.5 before:bg-teal-400 before:scale-x-0 hover:before:scale-x-100 focus:outline-none focus:ring-0 active:text-teal-400 active:before:bg-teal-400">
->>>>>>> 06d9fdd6dacfe9d4e4e10cfe34e6dae078e0db19
                 E-Learning
             </a>
         </li>
         <li>
             <a href="/bootcamp"
-<<<<<<< HEAD
-                class="text-nowrap {{ request()->is('bootcamp') ? 'text-teal-400 before:scale-x-100' : 'text-black before:scale-x-0' }} text-xs lg:text-sm font-medium relative px-2 py-2 rounded transition-all duration-300 before:absolute before:bottom-0 before:left-0 before:w-full before:h-0.5 before:bg-teal-400 hover:text-teal-400 hover:before:scale-x-100 focus:outline-none focus:ring-0">
-=======
                 class="text-nowrap {{ request()->is('bootcamp') ? 'text-teal-400 before:scale-x-100' : 'text-black before:scale-x-0' }} text-xs lg:text-md font-medium relative px-2 py-2 rounded transition-all duration-300 before:absolute before:bottom-0 before:left-0 before:w-full before:h-0.5 before:bg-teal-400 hover:text-teal-400 hover:before:scale-x-100 focus:outline-none focus:ring-0">
->>>>>>> 06d9fdd6dacfe9d4e4e10cfe34e6dae078e0db19
                 Program & Bootcamp
             </a>
         </li>
         <li>
             <a href="/review"
-<<<<<<< HEAD
-                class="text-nowrap {{ request()->is('review') ? 'text-teal-400 before:scale-x-100' : 'text-black before:scale-x-0' }} text-xs lg:text-sm font-medium hover:text-teal-400 relative px-2 py-2 rounded transition-all duration-300 before:absolute before:bottom-0 before:left-0 before:w-full before:h-0.5 before:bg-teal-400 before:scale-x-0 hover:before:scale-x-100 focus:outline-none focus:ring-0 active:text-teal-400 active:before:bg-teal-400">
-=======
                 class="text-nowrap {{ request()->is('review') ? 'text-teal-400 before:scale-x-100' : 'text-black before:scale-x-0' }} text-xs lg:text-md font-medium hover:text-teal-400 relative px-2 py-2 rounded transition-all duration-300 before:absolute before:bottom-0 before:left-0 before:w-full before:h-0.5 before:bg-teal-400 before:scale-x-0 hover:before:scale-x-100 focus:outline-none focus:ring-0 active:text-teal-400 active:before:bg-teal-400">
->>>>>>> 06d9fdd6dacfe9d4e4e10cfe34e6dae078e0db19
                 Review CV
             </a>
         </li>
         <li>
             <a href="/corporate-service"
-<<<<<<< HEAD
-                class="text-nowrap {{ request()->is('corporate-service') ? 'text-teal-400 before:scale-x-100' : 'text-black before:scale-x-0' }} text-xs lg:text-sm font-medium hover:text-teal-400 relative px-2 py-2 rounded transition-all duration-300 before:absolute before:bottom-0 before:left-0 before:w-full before:h-0.5 before:bg-teal-400 before:scale-x-0 hover:before:scale-x-100 focus:outline-none focus:ring-0 active:text-teal-400 active:before:bg-teal-400">
-=======
                 class="text-nowrap {{ request()->is('corporate-service') ? 'text-teal-400 before:scale-x-100' : 'text-black before:scale-x-0' }} lg:text-md text-xs font-medium hover:text-teal-400 relative px-2 py-2 rounded transition-all duration-300 before:absolute before:bottom-0 before:left-0 before:w-full before:h-0.5 before:bg-teal-400 before:scale-x-0 hover:before:scale-x-100 focus:outline-none focus:ring-0 active:text-teal-400 active:before:bg-teal-400">
->>>>>>> 06d9fdd6dacfe9d4e4e10cfe34e6dae078e0db19
                 Corporate Service
             </a>
         </li>
